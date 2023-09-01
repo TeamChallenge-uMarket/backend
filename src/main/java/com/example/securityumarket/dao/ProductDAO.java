3545830a19c5bf6f860946f966465b0a6b1d7a15
Dmytro Marzhiievskyi <freemanopposite@gmail.com>
2023-09-01 21:12:06 +0300

    add status

--- src/main/java/com/example/securityumarket/dao/ProductDAO.java
+++ src/main/java/com/example/securityumarket/dao/ProductDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface ProductDAO extends JpaRepository<Product, Long> {
     List<Product> findAllByUserId(Long id);
 
-    @Query("select p from Product p where upper(p.name) like lower(concat('%', :name, '%')) order by p.created desc")
+    @Query("select p from Product p where upper(p.name) like upper(concat('%', :name, '%')) order by p.created desc")
     List<Product> findProductsByPartOfName(@Param("name") String name, PageRequest pageable);
 
 
