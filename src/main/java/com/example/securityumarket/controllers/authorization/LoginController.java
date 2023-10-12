7a2ef98eee67232695550f035509c7a669326309
DenkovychOleh <praice07@gmail.com>
2023-10-12 20:19:11 +0300

    fix email sending bug

---
+++ src/main/java/com/example/securityumarket/controllers/authorization/LoginController.java
@@ -0,0 +1,2 @@
+package com.example.securityumarket.controllers.authorization;public class LoginController {
+}
---
+++ src/main/java/com/example/securityumarket/exception/EmailSendingException.java
@@ -0,0 +1,2 @@
+package com.example.securityumarket.exception;public class EmailSendingException {
+}
