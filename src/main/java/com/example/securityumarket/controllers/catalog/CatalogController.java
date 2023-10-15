6aa3976a90bc77e031874a6e871b5f698c880706
DenkovychOleh <praice07@gmail.com>
2023-10-15 14:05:38 +0300

    Added cors

--- src/main/java/com/example/securityumarket/controllers/catalog/CatalogController.java
+++ src/main/java/com/example/securityumarket/controllers/catalog/CatalogController.java
@@ -4,12 +4,10 @@ import com.example.securityumarket.services.page_service.CatalogPageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/catalog")
 @RequiredArgsConstructor
 @Tag(name = "Catalog page", description = "catalog page endpoints")
