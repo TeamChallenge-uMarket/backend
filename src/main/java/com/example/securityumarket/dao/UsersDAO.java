612e7bf3a38700868a9800e4e3c0a522b339dc3c
DenkovychOleh <praice07@gmail.com>
2023-10-24 23:05:15 +0300

    Added remove from DB of inactive users who have not accepted account within 30 minutes of registration

--- src/main/java/com/example/securityumarket/dao/UsersDAO.java
+++ src/main/java/com/example/securityumarket/dao/UsersDAO.java
@@ -2,8 +2,13 @@ package com.example.securityumarket.dao;
 
 import com.example.securityumarket.models.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +20,7 @@ public interface UsersDAO extends JpaRepository<Users, Long> {
 
     boolean existsUsersByPhone(String phone);
 
-    }
+    @Modifying
+    @Query("DELETE FROM Users u WHERE u.active = false AND u.created < :date")
+    void deleteByActiveFalseAndCreatedDateBefore(@Param("date")LocalDateTime created_date);
+}
