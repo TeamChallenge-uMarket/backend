abb104c521338f4a3d78f188d6f2d84aa3a3a382
Denkovych Oleh <praice07@gmail.com>
2024-01-03 19:28:24 +0200

    updated UnauthenticatedException

--- src/main/java/com/example/securityumarket/exception/UnauthenticatedException.java
+++ src/main/java/com/example/securityumarket/exception/UnauthenticatedException.java
@@ -1,10 +1,5 @@
 package com.example.securityumarket.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
-import java.time.Instant;
-
-@Slf4j
 public class UnauthenticatedException extends RuntimeException  {
     public UnauthenticatedException(String message) {
         super(message);
