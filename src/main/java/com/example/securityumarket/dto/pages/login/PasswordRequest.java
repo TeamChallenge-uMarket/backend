72a328ad4ec34c81d08e034a5b5329b0942eb901
WallRun <dimonty2003@gmail.com>
2024-02-08 21:06:51 +0200

    Added ~ symbol to password validation regex

--- src/main/java/com/example/securityumarket/dto/pages/login/PasswordRequest.java
+++ src/main/java/com/example/securityumarket/dto/pages/login/PasswordRequest.java
@@ -29,7 +29,7 @@ public class PasswordRequest {
         if (password == null || confirmPassword == null) {
             return false;
         }
-        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d#?!@$%^&*(){}:;_+|<>/.,'=№`\\-\"\\[\\]]{8,49}")
+        return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d#?!@$%^&~*(){}:;_+|<>/.,'=№`\\-\"\\[\\]]{8,49}")
                 && password.equals(confirmPassword);
     }
 
